60a871c0ac65b79d36817ca0685fe482e7d33135
Артем Баторшин <norvellart@gmail.com>
2022-02-23 16:56:23 +0300

    added homework42

--- homework42/articles/view.py
+++ homework42/articles/view.py
@@ -1,14 +1,27 @@
+def add_title(title):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            print(title.center(50, "="))
+            res = func(*args, **kwargs)
+            print("=" * 50)
+            return res
+
+        return wrapper
+
+    return decorator
+
+
 class UserInterface:
+    @add_title(' Ввод пользовательских данных ')
     def wait_user_answer(self):
-        print(' Ввод пользовательских данных '.center(50, '='))
         print('Действия со статьями:')
         print('1 - создание статьи'
               '\n2 - просмотр статей'
               '\nq - выход из программы')
         user_answer = input('Выберите вариант действия: ')
-        print('=' * 50)
         return user_answer
 
+    @add_title(' Создание статьи: ')
     def add_user_articles(self):
         dict_article = {
             'название': None,
@@ -16,14 +29,11 @@ class UserInterface:
             'количество страниц': None,
             'описание': None
         }
-        print(' Создание статьи: '.center(50, '='))
         for key in dict_article:
             dict_article[key] = input(f'Введите {key} статьи: ')
-        print('=' * 50)
         return dict_article
 
+    @add_title(' Список статей: ')
     def show_all_articles(self, articles):
-        print(' Список статей: '.center(50, '='))
         for ind, article in enumerate(articles, 1):
             print(f'{ind}. {article}')
-        print('=' * 50)
